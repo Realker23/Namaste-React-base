13e7bc90016159e3c5e4b78419586d28aa69cdfa
Abhishek Roushan <abhishekashu23@gmail.com>
2024-02-06 22:14:22 +0530

    Updates

---
+++ Grocery.be0edbd2.js
@@ -0,0 +1,3 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire164e;e.register("loR3J",function(o,n){Object.defineProperty(o.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(o.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),l=()=>/*#__PURE__*/(0,i.jsx)("h1",{children:"This is online grocery store along with the different component for this"})});//# sourceMappingURL=Grocery.be0edbd2.js.map
+
+//# sourceMappingURL=Grocery.be0edbd2.js.map
